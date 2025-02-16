661f900e70c2f3f04f7a300c71d5328c576bbfdb
Soonil Hong <s.hong.5181@gmail.com>
2025-02-16 13:57:45 -0500

    [Extensions] Add initial implementation of StringExtensions

---
+++ UltimateUtils/Extensions/StringExtensions.cs
@@ -0,0 +1,34 @@
+namespace UltimateUtils.Extensions;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrEmpty(this string arg)
+    {
+        return string.IsNullOrEmpty(arg);
+    }
+
+    public static bool IsNullOrWhiteSpace(this string arg)
+    {
+        return string.IsNullOrWhiteSpace(arg);
+    }
+
+    public static bool IsNotNullOrEmpty(this string arg)
+    {
+        return !string.IsNullOrEmpty(arg);
+    }
+
+    public static bool IsNotNullOrWhiteSpace(this string arg)
+    {
+        return !string.IsNullOrWhiteSpace(arg);
+    }
+
+    public static string JoinToString(this IEnumerable<string> items, string separator = "")
+    {
+        return string.Join(separator, items);
+    }
+
+    public static string JoinToString<T>(this IEnumerable<T> items, string separator = "")
+    {
+        return string.Join(separator, items);
+    }
+}
