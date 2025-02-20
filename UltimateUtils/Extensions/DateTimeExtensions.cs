a7c5833a4a59e699ba848df4dfe5bc9082489712
Soonil Hong <s.hong.5181@gmail.com>
2025-02-20 18:39:01 -0500

    [UltimateUtils] Add DateTimeExtensions with initial implementation

---
+++ UltimateUtils/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,119 @@
+namespace UltimateUtils.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static DateTime January(this int year, int day)
+    {
+        return new DateTime(year, 1, day);
+    }
+
+    public static DateTime Jan(this int year, int day)
+    {
+        return new DateTime(year, 1, day);
+    }
+
+    public static DateTime February(this int year, int day)
+    {
+        return new DateTime(year, 2, day);
+    }
+
+    public static DateTime Feb(this int year, int day)
+    {
+        return new DateTime(year, 2, day);
+    }
+
+    public static DateTime March(this int year, int day)
+    {
+        return new DateTime(year, 3, day);
+    }
+
+    public static DateTime Mar(this int year, int day)
+    {
+        return new DateTime(year, 3, day);
+    }
+
+    public static DateTime April(this int year, int day)
+    {
+        return new DateTime(year, 4, day);
+    }
+
+    public static DateTime Apr(this int year, int day)
+    {
+        return new DateTime(year, 4, day);
+    }
+
+    public static DateTime May(this int year, int day)
+    {
+        return new DateTime(year, 5, day);
+    }
+
+    public static DateTime June(this int year, int day)
+    {
+        return new DateTime(year, 6, day);
+    }
+
+    public static DateTime Jun(this int year, int day)
+    {
+        return new DateTime(year, 6, day);
+    }
+
+    public static DateTime July(this int year, int day)
+    {
+        return new DateTime(year, 7, day);
+    }
+
+    public static DateTime Jul(this int year, int day)
+    {
+        return new DateTime(year, 7, day);
+    }
+
+    public static DateTime August(this int year, int day)
+    {
+        return new DateTime(year, 8, day);
+    }
+
+    public static DateTime Aug(this int year, int day)
+    {
+        return new DateTime(year, 8, day);
+    }
+
+    public static DateTime September(this int year, int day)
+    {
+        return new DateTime(year, 9, day);
+    }
+
+    public static DateTime Sep(this int year, int day)
+    {
+        return new DateTime(year, 9, day);
+    }
+
+    public static DateTime October(this int year, int day)
+    {
+        return new DateTime(year, 10, day);
+    }
+
+    public static DateTime Oct(this int year, int day)
+    {
+        return new DateTime(year, 10, day);
+    }
+
+    public static DateTime November(this int year, int day)
+    {
+        return new DateTime(year, 11, day);
+    }
+
+    public static DateTime Nov(this int year, int day)
+    {
+        return new DateTime(year, 11, day);
+    }
+
+    public static DateTime December(this int year, int day)
+    {
+        return new DateTime(year, 12, day);
+    }
+
+    public static DateTime Dec(this int year, int day)
+    {
+        return new DateTime(year, 12, day);
+    }
+}
