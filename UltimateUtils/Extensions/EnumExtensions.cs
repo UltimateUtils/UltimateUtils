85febdc296922a9fff4c2fb3dba17c65889765f0
Soonil Hong <s.hong.5181@gmail.com>
2025-02-26 00:15:54 -0500

    [UltimateUtils 1.4.4] Support ignoreCase for ConvertEnum

--- UltimateUtils/Extensions/EnumExtensions.cs
+++ UltimateUtils/Extensions/EnumExtensions.cs
@@ -13,17 +13,17 @@ public static class EnumExtensions
         throw new ArgumentException($"Cannot convert {input} to a value of the {typeof(TEnum).FullName} enumeration.");
     }
 
-    public static TEnumTo ConvertEnum<TEnumFrom, TEnumTo>(this TEnumFrom input)
+    public static TEnumTo ConvertEnum<TEnumFrom, TEnumTo>(this TEnumFrom input, bool ignoreCase = false)
         where TEnumFrom : struct, Enum
         where TEnumTo : struct, Enum
     {
-        return input.ToString().ToEnum<TEnumTo>();
+        return input.ToString().ToEnum<TEnumTo>(ignoreCase);
     }
 
-    public static TEnumTo? ConvertEnum<TEnumFrom, TEnumTo>(this TEnumFrom? input)
+    public static TEnumTo? ConvertEnum<TEnumFrom, TEnumTo>(this TEnumFrom? input, bool ignoreCase = false)
         where TEnumFrom : struct, Enum
         where TEnumTo : struct, Enum
     {
-        return input?.ToString().ToEnum<TEnumTo>();
+        return input?.ToString().ToEnum<TEnumTo>(ignoreCase);
     }
 }
