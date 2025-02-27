e86ed3f0027b4b2983d064c3d9aaf4fa3f5f678b
Soonil Hong <s.hong.5181@gmail.com>
2025-02-27 11:41:52 -0500

    [UltimateUtils 1.4.8] Refactor EnumExtensions

--- UltimateUtils/Extensions/EnumExtensions.cs
+++ UltimateUtils/Extensions/EnumExtensions.cs
@@ -5,25 +5,34 @@ public static class EnumExtensions
     public static TEnum ToEnum<TEnum>(this string input, bool ignoreCase = false)
         where TEnum : struct, Enum
     {
-        if (Enum.TryParse(input, ignoreCase, out TEnum result))
-        {
-            return result;
-        }
-
-        throw new ArgumentException($"Cannot convert {input} to a value of the {typeof(TEnum).FullName} enumeration.");
+        return input._ToEnum<TEnum>(ignoreCase);
     }
 
     public static TEnumTo ConvertEnum<TEnumFrom, TEnumTo>(this TEnumFrom input, bool ignoreCase = false)
         where TEnumFrom : struct, Enum
         where TEnumTo : struct, Enum
     {
-        return input.ToString().ToEnum<TEnumTo>(ignoreCase);
+        return input.ToString()._ToEnum<TEnumTo>(ignoreCase);
     }
 
     public static TEnumTo? ConvertEnum<TEnumFrom, TEnumTo>(this TEnumFrom? input, bool ignoreCase = false)
         where TEnumFrom : struct, Enum
         where TEnumTo : struct, Enum
     {
-        return input?.ToString().ToEnum<TEnumTo>(ignoreCase);
+        return input?.ToString()._ToEnum<TEnumTo>(ignoreCase);
     }
+
+    #region private
+
+    private static TEnum _ToEnum<TEnum>(this string input, bool ignoreCase) where TEnum : struct, Enum
+    {
+        if (Enum.TryParse(input, ignoreCase, out TEnum result))
+        {
+            return result;
+        }
+
+        throw new ArgumentException($"Cannot convert {input ?? "null"} to the {typeof(TEnum).FullName} enum value.");
+    }
+
+    #endregion
 }
