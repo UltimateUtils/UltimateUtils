c1fe7a34b5c749286f1da1ddf7d69317b9211c31
Soonil Hong <s.hong.5181@gmail.com>
2025-02-27 10:03:22 -0500

    [HelloUltimateUtils] Add UMath ref

--- HelloUltimateUtils/Program.cs
+++ HelloUltimateUtils/Program.cs
@@ -2,25 +2,6 @@
 using UltimateUtils.EF.Pagination;
 using UltimateUtils.Extensions;
 using UltimateUtils.Pagination;
+using UltimateUtils.Utils;
 
 Console.WriteLine("Hello, UltimateUtils!");
-
-var x = GetNumbers();
-var y = GetStudents();
-
-return;
-
-IEnumerable<int> GetNumbers()
-{
-    yield return 1;
-    yield return 2;
-    yield return 3;
-    yield return 4;
-}
-
-IEnumerable<Student> GetStudents()
-{
-    yield return new Student(1, "John", "Doe");
-    yield return new Student(2, "Jane", "Doe");
-    yield return new Student(3, "Tom", "Cruise");
-}
