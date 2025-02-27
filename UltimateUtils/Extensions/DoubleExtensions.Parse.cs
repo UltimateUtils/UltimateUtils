c90b8fdb8cbfd5005c2e23ed723a213f1f534101
Soonil Hong <s.hong.5181@gmail.com>
2025-02-27 18:57:36 -0500

    [UltimateUtils 1.4.16] Add more Parse implementations to DoubleExtensions

--- UltimateUtils/Extensions/DoubleExtensions.Parse.cs
+++ UltimateUtils/Extensions/DoubleExtensions.Parse.cs
@@ -5,6 +5,8 @@ namespace UltimateUtils.Extensions;
 
 public static partial class DoubleExtensions
 {
+    #region Parse
+
     public static double ParseToDouble(this string number)
     {
         return double.Parse(number);
@@ -30,25 +32,51 @@ public static partial class DoubleExtensions
 
     public static double ParseToDouble(
         this ReadOnlySpan<char> number,
+        IFormatProvider? provider)
+    {
+        return double.Parse(number, provider);
+    }
+
+    public static double ParseToDouble(
+        this ReadOnlySpan<char> number,
         NumberStyles style = NumberStyles.Float | NumberStyles.AllowThousands,
         IFormatProvider? provider = null)
     {
         return double.Parse(number, style, provider);
     }
 
-    public static bool TryParse([NotNullWhen(true)] this string? number, out double result)
+    public static double ParseToDouble(
+        this ReadOnlySpan<byte> number,
+        IFormatProvider? provider)
+    {
+        return double.Parse(number, provider);
+    }
+
+    public static double ParseToDouble(
+        this ReadOnlySpan<byte> number,
+        NumberStyles style = NumberStyles.Float | NumberStyles.AllowThousands,
+        IFormatProvider? provider = null)
+    {
+        return double.Parse(number, style, provider);
+    }
+
+    #endregion Parse
+
+    #region TryParse
+
+    public static bool TryParse(
+        [NotNullWhen(true)] this string? number,
+        out double result)
     {
         return double.TryParse(number, out result);
     }
 
-    public static bool TryParse(this ReadOnlySpan<char> number, out double result)
+    public static bool TryParse(
+        [NotNullWhen(true)] this string? number,
+        IFormatProvider? provider,
+        out double result)
     {
-        return double.TryParse(number, out result);
-    }
-
-    public static bool TryParse(this ReadOnlySpan<byte> number, out double result)
-    {
-        return double.TryParse(number, out result);
+        return double.TryParse(number, provider, out result);
     }
 
     public static bool TryParse(
@@ -66,6 +94,21 @@ public static partial class DoubleExtensions
 
     public static bool TryParse(
         this ReadOnlySpan<char> number,
+        out double result)
+    {
+        return double.TryParse(number, out result);
+    }
+
+    public static bool TryParse(
+        this ReadOnlySpan<char> number,
+        IFormatProvider? provider,
+        out double result)
+    {
+        return double.TryParse(number, provider, out result);
+    }
+
+    public static bool TryParse(
+        this ReadOnlySpan<char> number,
         NumberStyles style,
         IFormatProvider? provider,
         out double result)
@@ -76,4 +119,30 @@ public static partial class DoubleExtensions
             provider,
             out result);
     }
+
+    public static bool TryParse(
+        this ReadOnlySpan<byte> number,
+        out double result)
+    {
+        return double.TryParse(number, out result);
+    }
+
+    public static bool TryParse(
+        this ReadOnlySpan<byte> number,
+        IFormatProvider? provider,
+        out double result)
+    {
+        return double.TryParse(number, provider, out result);
+    }
+
+    public static bool TryParse(
+        this ReadOnlySpan<byte> number,
+        NumberStyles style,
+        IFormatProvider? provider,
+        out double result)
+    {
+        return double.TryParse(number, style, provider, out result);
+    }
+
+    #endregion TryParse
 }
