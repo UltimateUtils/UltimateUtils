d27a79a8b1dcea12159232afca9804f0d73d2d4d
Soonil Hong <s.hong.5181@gmail.com>
2025-03-07 16:19:21 -0500

    [UltimateUtils 1.4.22] Add consts for pagination headers

---
+++ UltimateUtils/Pagination/Constants.cs
@@ -0,0 +1,12 @@
+namespace UltimateUtils.Pagination;
+
+public static class Constants
+{
+    public const string XTotalCount = "x-total-count";
+
+    public const string XTotalPages = "x-total-pages";
+
+    public const string XHasPreviousPage = "x-has-previous-page";
+
+    public const string XHasNextPage = "x-has-next-page";
+}
