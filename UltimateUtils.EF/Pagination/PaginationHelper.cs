ba91f80fb1fdbbd4ddf9b06cc37c8d9cb47696fa
Soonil Hong <s.hong.5181@gmail.com>
2025-03-11 12:28:13 -0400

    [UltimateUtils.EF 2.0.1] Add more paginate helper methods

--- UltimateUtils.EF/Pagination/PaginationHelper.cs
+++ UltimateUtils.EF/Pagination/PaginationHelper.cs
@@ -160,4 +160,91 @@ public static class PaginationHelper
     }
 
     #endregion
+
+    #region PaginateAsync 2 -> PagedOrderedList
+
+    public static async Task<IPagedList<T>> PaginateAsync<T, TKey>(
+        this IQueryable<T> sourceItems,
+        Expression<Func<T, TKey>> orderByKeySelectorExpression,
+        bool descending,
+        int pageNumber,
+        int pageSize,
+        CancellationToken cancellationToken = default)
+    {
+        int totalItemCount = await sourceItems.CountAsync(cancellationToken).ConfigureAwait(false);
+
+        if (totalItemCount <= 0)
+        {
+            return PagedOrderedList<T, TKey>.Create(
+                pageNumber,
+                pageSize,
+                totalItemCount,
+                []);
+        }
+
+        var currentItems =
+            descending
+                ? await sourceItems
+                    .OrderByDescending(orderByKeySelectorExpression)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken)
+                    .ConfigureAwait(false)
+                : await sourceItems
+                    .OrderBy(orderByKeySelectorExpression)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+        return PagedOrderedList<T, TKey>.Create(
+            pageNumber,
+            pageSize,
+            totalItemCount,
+            currentItems);
+    }
+
+    public static async Task<IPagedList<TResult>> PaginateAsync<TSource, TKey, TResult>(
+        this IQueryable<TSource> sourceItems,
+        Expression<Func<TSource, TKey>> orderByKeySelectorExpression,
+        bool descending,
+        int pageNumber,
+        int pageSize,
+        Func<TSource, TResult> converter,
+        CancellationToken cancellationToken = default)
+    {
+        int totalItemCount = await sourceItems.CountAsync(cancellationToken).ConfigureAwait(false);
+
+        if (totalItemCount <= 0)
+        {
+            return PagedOrderedList<TSource, TKey, TResult>.Create(
+                pageNumber,
+                pageSize,
+                totalItemCount,
+                []);
+        }
+
+        var currentItems =
+            descending
+                ? await sourceItems
+                    .OrderByDescending(orderByKeySelectorExpression)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken)
+                    .ConfigureAwait(false)
+                : await sourceItems
+                    .OrderBy(orderByKeySelectorExpression)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+        return PagedOrderedList<TSource, TKey, TResult>.Create(
+            pageNumber,
+            pageSize,
+            totalItemCount,
+            currentItems.Select(converter));
+    }
+
+    #endregion
 }
