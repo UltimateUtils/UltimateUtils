8c8ff2236d77ff64b162ba0a1b0fa7abbb6522b5
Soonil Hong <s.hong.5181@gmail.com>
2025-04-04 11:06:16 -0400

    [UltimateUtils 2.1.4] Add PagedListWrapper extensions for setting response headers

--- UltimateUtils/Pagination/ResponseHeaderHelper.cs
+++ UltimateUtils/Pagination/ResponseHeaderHelper.cs
@@ -40,4 +40,40 @@ public static class ResponseHeaderHelper
         responseHeaders.Append(headerOptions.XHasPreviousPage, pagedList.HasPreviousPage.ToString());
         responseHeaders.Append(headerOptions.XHasNextPage, pagedList.HasNextPage.ToString());
     }
+
+    public static PagedListWrapper<T> SettingPaginationHeaders<T>(
+        this PagedListWrapper<T> pagedList,
+        IHeaderDictionary responseHeaders,
+        Action<HeaderOptions>? action = null)
+    {
+        var headerOptions = new HeaderOptions();
+        action?.Invoke(headerOptions);
+
+        responseHeaders.Append(headerOptions.XCurrentCount, pagedList.Items.ToString());
+        responseHeaders.Append(headerOptions.XTotalCount, pagedList.TotalItemCount.ToString());
+        responseHeaders.Append(headerOptions.XTotalPages, pagedList.TotalPageCount.ToString());
+        responseHeaders.Append(headerOptions.XPageNumber, pagedList.PageNumber.ToString());
+        responseHeaders.Append(headerOptions.XPageSize, pagedList.PageSize.ToString());
+        responseHeaders.Append(headerOptions.XHasPreviousPage, pagedList.HasPreviousPage.ToString());
+        responseHeaders.Append(headerOptions.XHasNextPage, pagedList.HasNextPage.ToString());
+
+        return pagedList;
+    }
+
+    public static void SetPaginationHeaders<T>(
+        this IHeaderDictionary responseHeaders,
+        PagedListWrapper<T> pagedList,
+        Action<HeaderOptions>? action = null)
+    {
+        var headerOptions = new HeaderOptions();
+        action?.Invoke(headerOptions);
+
+        responseHeaders.Append(headerOptions.XCurrentCount, pagedList.Items.ToString());
+        responseHeaders.Append(headerOptions.XTotalCount, pagedList.TotalItemCount.ToString());
+        responseHeaders.Append(headerOptions.XTotalPages, pagedList.TotalPageCount.ToString());
+        responseHeaders.Append(headerOptions.XPageNumber, pagedList.PageNumber.ToString());
+        responseHeaders.Append(headerOptions.XPageSize, pagedList.PageSize.ToString());
+        responseHeaders.Append(headerOptions.XHasPreviousPage, pagedList.HasPreviousPage.ToString());
+        responseHeaders.Append(headerOptions.XHasNextPage, pagedList.HasNextPage.ToString());
+    }
 }
