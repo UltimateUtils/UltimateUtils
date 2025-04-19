31257b087a64448902a4f3f108eaeb199605bdce
Soonil Hong <s.hong.5181@gmail.com>
2025-04-19 15:23:28 -0400

    [UltimateUtils 2.1.9] Update EnsureNotNull/EnsuringNotNull

--- UltimateUtils/Extensions/GenericObjectExtensions.cs
+++ UltimateUtils/Extensions/GenericObjectExtensions.cs
@@ -3,11 +3,13 @@ namespace UltimateUtils.Extensions;
 public static class GenericObjectExtensions
 {
     public static void EnsureNotNull<T>(this T? argument, string? paramName = null)
+        where T : class
     {
         ArgumentNullException.ThrowIfNull(argument, paramName);
     }
 
     public static T EnsuringNotNull<T>(this T? argument, string? paramName = null)
+        where T : class
     {
         ArgumentNullException.ThrowIfNull(argument, paramName);
 
