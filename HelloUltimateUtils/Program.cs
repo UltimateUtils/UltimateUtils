5de7910403fe474b86099548c42d9fd09d88add1
Soonil Hong <s.hong.5181@gmail.com>
2025-07-26 18:48:39 -0400

    [UltimateUtils] Remove Pagination from UU

--- HelloUltimateUtils/Program.cs
+++ HelloUltimateUtils/Program.cs
@@ -1,8 +1,6 @@
 ﻿using HelloUltimateUtils;
 using UltimatePagination;
-using UltimateUtils.EF.Pagination;
 using UltimateUtils.Extensions;
-using UltimateUtils.Pagination;
 using UltimateUtils.Utils;
 
 Console.WriteLine("Hello, UltimateUtils!");
