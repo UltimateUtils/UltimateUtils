aac658b8c91395a7979bf134ee5d2b67802c5bcc
Soonil Hong <s.hong.5181@gmail.com>
2026-09-14 19:50:03 -0400

    [UltimateFlags] Add test projects

---
+++ UltimateFlags/Tests/UltimateFlags.EF.UnitTests/FlagServiceTests.cs
@@ -0,0 +1,15 @@
+namespace UltimateFlags.EF.UnitTests;
+
+public class FlagServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+}
