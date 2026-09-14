0825338854e84830da2686b5fbbf65fe2609c342
Soonil Hong <s.hong.5181@gmail.com>
2026-09-13 23:10:31 -0400

    [UltimateFlags] Add migrations for examples

--- UltimateFlags/UltimateFlags.EF/db_add_migration.sh
+++ UltimateFlags/UltimateFlags.EF/db_add_migration.sh
@@ -3,4 +3,4 @@
 # Run this script from your Host project's directory
 # after installing Microsoft.EntityFrameworkCore.Design nuget package
 
-dotnet ef migrations add <MigrationName> -o Data/Migrations
+dotnet ef migrations add <MigrationName> -o Db/Migrations
