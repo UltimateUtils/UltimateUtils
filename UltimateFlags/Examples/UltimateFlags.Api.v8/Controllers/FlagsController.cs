4872828b2f501da377353fd39a46bc07e5c72c3a
Soonil Hong <s.hong.5181@gmail.com>
2026-09-14 21:23:08 -0400

    [UltimateFlags] Add Example controller skeleton

---
+++ UltimateFlags/Examples/UltimateFlags.Api.v8/Controllers/FlagsController.cs
@@ -0,0 +1,151 @@
+using Microsoft.AspNetCore.Mvc;
+using UltimateFlags.Abstraction.Contracts;
+using UltimateFlags.Api.v8.Services.Abstraction;
+using UltimatePagination.Abstraction;
+
+namespace UltimateFlags.Api.v8.Controllers;
+
+[ApiController]
+[Route("flags")]
+public class FlagsController : ControllerBase
+{
+    private readonly ILogger<FlagsController> _logger;
+
+    private readonly IFlagService _flagService;
+
+    public FlagsController(
+        ILogger<FlagsController> logger,
+        IFlagService flagService)
+    {
+        _logger = logger;
+        _flagService = flagService;
+    }
+
+    [HttpPost]
+    [Route("")]
+    public FlagResponse Create(FlagCreationRequest contract)
+    {
+        return _flagService.Create(contract);
+    }
+
+    [HttpGet]
+    [Route("{id:guid}")]
+    public FlagResponse GetById([FromRoute] Guid id)
+    {
+        return _flagService.GetRequired(id);
+    }
+
+    [HttpGet]
+    [Route("")]
+    public FlagResponse Get([FromQuery] string name, [FromQuery] Guid? parentId)
+    {
+        return _flagService.GetRequired(name, parentId);
+    }
+
+    [HttpGet]
+    [Route("{key}")]
+    public FlagResponse Get([FromRoute] string key)
+    {
+        return _flagService.GetRequired(key);
+    }
+
+    [HttpGet]
+    [Route("list")]
+    public IPagedList<FlagResponse> List(
+        [FromQuery] string? searchString = null,
+        [FromQuery] bool? isOn = null,
+        [FromQuery] int pageNumber = 1,
+        [FromQuery] int pageSize = 20)
+    {
+        return
+            _flagService
+                .List(
+                    searchString,
+                    isOn,
+                    pageNumber,
+                    pageSize);
+    }
+
+    [HttpPut]
+    [Route("{id:guid}")]
+    public FlagResponse Update([FromRoute] Guid id, [FromBody] FlagUpdateRequest contract)
+    {
+        return _flagService.Update(id, contract);
+    }
+
+    [HttpPut]
+    [Route("execute-update/{id:guid}")]
+    public int ExecuteUpdate([FromRoute] Guid id, [FromBody] FlagUpdateRequest contract)
+    {
+        return _flagService.ExecuteUpdate(id, contract);
+    }
+
+    [HttpDelete]
+    [Route("{id:guid}")]
+    public FlagResponse Delete([FromRoute] Guid id, [FromQuery] bool purge = false)
+    {
+        return
+            purge
+                ? _flagService.Purge(id)
+                : _flagService.Delete(id);
+    }
+
+    [HttpDelete]
+    [Route("execute-delete/{id:guid}")]
+    public int ExecuteDelete([FromRoute] Guid id, [FromQuery] bool purge = false)
+    {
+        return
+            purge
+                ? _flagService.ExecutePurge(id)
+                : _flagService.ExecuteDelete(id);
+    }
+
+    [HttpDelete]
+    [Route("execute-purge")]
+    public int ExecutePurge([FromQuery] DateTime? from, [FromQuery] DateTime? to)
+    {
+        return _flagService.ExecutePurge(from, to);
+    }
+
+    [HttpPut]
+    [Route("{id:guid}/enable")]
+    public void EnableById([FromRoute] Guid id)
+    {
+        _flagService.Enable(id);
+    }
+
+    [HttpPut]
+    [Route("enable")]
+    public void EnableByName([FromQuery] string name, [FromQuery] Guid? parentId)
+    {
+        _flagService.Enable(name, parentId);
+    }
+
+    [HttpPut]
+    [Route("{id:guid}/disable")]
+    public void DisableById([FromRoute] Guid id)
+    {
+        _flagService.Disable(id);
+    }
+
+    [HttpPut]
+    [Route("disable")]
+    public void DisableByName([FromQuery] string name, [FromQuery] Guid? parentId)
+    {
+        _flagService.Disable(name, parentId);
+    }
+
+    [HttpGet]
+    [Route("{key}/is-on")]
+    public bool IsOnByKey([FromRoute] string key)
+    {
+        return _flagService.IsOn(key);
+    }
+
+    [HttpGet]
+    [Route("{id:guid}/is-on")]
+    public bool IsOnByKey([FromRoute] Guid id)
+    {
+        return _flagService.IsOn(id);
+    }
+}
---
+++ UltimateFlags/Examples/UltimateFlags.Api.v9/Services/FlagService.cs
@@ -0,0 +1,133 @@
+using UltimateFlags.Abstraction.Contracts;
+using UltimateFlags.Api.v9.Services.Abstraction;
+using UltimatePagination.Abstraction;
+
+namespace UltimateFlags.Api.v9.Services;
+
+public class FlagService : IFlagService
+{
+    private readonly ILogger<FlagService> _logger;
+
+    private readonly UltimateFlags.Abstraction.Services.IFlagService _flagService;
+
+    public FlagService(
+        ILogger<FlagService> logger,
+        UltimateFlags.Abstraction.Services.IFlagService flagService)
+    {
+        _logger = logger;
+        _flagService = flagService;
+    }
+
+    public FlagResponse Create(FlagCreationRequest contract)
+    {
+        return _flagService.Create(contract);
+    }
+
+    public FlagResponse? Get(Guid id)
+    {
+        return _flagService.Get(id);
+    }
+
+    public FlagResponse GetRequired(Guid id)
+    {
+        return _flagService.GetRequired(id);
+    }
+
+    public FlagResponse? Get(string name, Guid? parentId)
+    {
+        return _flagService.Get(name, parentId);
+    }
+
+    public FlagResponse GetRequired(string name, Guid? parentId)
+    {
+        return _flagService.GetRequired(name, parentId);
+    }
+
+    public FlagResponse? Get(string key)
+    {
+        return _flagService.Get(key);
+    }
+
+    public FlagResponse GetRequired(string key)
+    {
+        return _flagService.GetRequired(key);
+    }
+
+    public IPagedList<FlagResponse> List(
+        string? searchString = null,
+        bool? isOn = null,
+        int pageNumber = 1,
+        int pageSize = 20)
+    {
+        return _flagService.List(
+            searchString,
+            isOn,
+            pageNumber,
+            pageSize);
+    }
+
+    public FlagResponse Update(Guid id, FlagUpdateRequest contract)
+    {
+        return _flagService.Update(id, contract);
+    }
+
+    public int ExecuteUpdate(Guid id, FlagUpdateRequest contract)
+    {
+        return _flagService.ExecuteUpdate(id, contract);
+    }
+
+    public FlagResponse Delete(Guid id)
+    {
+        return _flagService.Delete(id);
+    }
+
+    public int ExecuteDelete(Guid id)
+    {
+        return _flagService.ExecuteDelete(id);
+    }
+
+    public FlagResponse Purge(Guid id)
+    {
+        return _flagService.Purge(id);
+    }
+
+    public int ExecutePurge(Guid id)
+    {
+        return _flagService.ExecutePurge(id);
+    }
+
+    public int ExecutePurge(DateTime? fromInclusive = null, DateTime? toInclusive = null)
+    {
+        return _flagService.ExecutePurge(fromInclusive, toInclusive);
+    }
+
+    public void Enable(Guid id)
+    {
+        _flagService.Enable(id);
+    }
+
+    public void Enable(string name, Guid? parentId)
+    {
+        _flagService.Enable(name, parentId);
+    }
+
+    public void Disable(Guid id)
+    {
+        _flagService.Disable(id);
+    }
+
+    public void Disable(string name, Guid? parentId)
+    {
+        _flagService.Disable(name, parentId);
+    }
+
+    public bool IsOn(string key)
+    {
+        return _flagService.IsOn(key);
+    }
+
+    public bool IsOn(Guid id)
+    {
+        return _flagService.IsOn(id);
+    }
+}
